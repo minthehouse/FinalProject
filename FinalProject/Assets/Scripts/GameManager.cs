0df3b2ace8620aaa476d99a8ae69eb705c22e105
Hangill Chong <47836580+oneway1222@users.noreply.github.com>
2020-11-23 23:32:36 -0500

    Items
    Made Coin, Power, Finish Items

--- FinalProject/Assets/Scripts/GameManager.cs
+++ FinalProject/Assets/Scripts/GameManager.cs
@@ -15,6 +15,7 @@ public class GameManager : MonoBehaviour
     public GameObject player;
     public Text scoreText;
     public Image[] lifeImage;
+    public Image[] boomImage;
     public GameObject gameOverSet;
 
     void Update()
@@ -67,6 +68,19 @@ public class GameManager : MonoBehaviour
         //UI Life Active
         for(int index=0; index<life; index++){
             lifeImage[index].color = new Color(1,1,1,1);
+        }
+    }
+
+    public void UpdateBoomIcon(int boom)
+    {
+        //UI Boom Init Disable
+        for(int index=0; index<3; index++){
+            boomImage[index].color = new Color(1,1,1,0);
+        }
+
+        //UI Boom Active
+        for(int index=0; index<boom; index++){
+            boomImage[index].color = new Color(1,1,1,1);
         }
     }
 
