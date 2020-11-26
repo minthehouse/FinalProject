688835bfc5a2ab33f110b364624340e6d0323ff3
Hangill Chong <47836580+oneway1222@users.noreply.github.com>
2020-11-25 21:27:22 -0500

    Enemy's location
    Set Enemy's location

--- FinalProject/Assets/Scripts/GameManager.cs
+++ FinalProject/Assets/Scripts/GameManager.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System.IO;
 
 public class GameManager : MonoBehaviour
 {
     public string[] enemyObjs;
     public Transform[] spawnPoints;
 
-    public float maxSpawnDelay;
+    public float nextSpawnDelay;
     public float curSpawnDelay;
 
     public GameObject player;
@@ -19,50 +20,109 @@ public class GameManager : MonoBehaviour
     public GameObject gameOverSet;
     public ObjectManager objectManager;
 
+    public List<Spawn> spawnList;
+    public int spawnIndex;
+    public bool spawnEnd;
+
     void Awake()
     {
+        spawnList = new List<Spawn>();
         enemyObjs = new string[]{ "EnemyS", "EnemyM", "EnemyL"};
+        ReadSpawnFile();
+    }
+
+    void ReadSpawnFile()
+    {
+        spawnList.Clear();
+        spawnIndex =0;
+        spawnEnd = false;
+
+        TextAsset textFile = Resources.Load("Stage 0") as TextAsset;
+        StringReader stringReader = new StringReader(textFile.text);
+
+        while(stringReader != null)
+        {
+            string line = stringReader.ReadLine();
+            Debug.Log(line);
+
+            if (line == null)
+                break;
+
+            Spawn spawnData = new Spawn();
+            spawnData.delay = float.Parse(line.Split(',')[0]);
+            spawnData.type = line.Split(',')[1];
+            spawnData.point = int.Parse(line.Split(',')[2]);
+            spawnList.Add(spawnData);
+        }
+
+        stringReader.Close();
+
+        nextSpawnDelay = spawnList[0].delay;
+
     }
 
     void Update()
     {
         curSpawnDelay += Time.deltaTime;
 
-        if(curSpawnDelay > maxSpawnDelay){
+        if(curSpawnDelay > nextSpawnDelay && !spawnEnd){
             SpawnEnemy();
-            maxSpawnDelay = Random.Range(0.5f,3f);
             curSpawnDelay = 0;
         }
 
-        //#.UI Score Update
+        //UI Score Update
         Player playerLogic = player.GetComponent<Player>();
         scoreText.text = string.Format("{0:n0}", playerLogic.score);
     }
 
+
+
     void SpawnEnemy()
     {
-        int ranEnemy = Random.Range(0,3);
-        int ranPoint = Random.Range(0,9);           //location enemies shows up -- 5 locations
-        GameObject enemy = objectManager.MakeObj(enemyObjs[ranEnemy]);
-        enemy.transform.position = spawnPoints[ranPoint].position;
+        int enemyIndex =0;
+        switch (spawnList[spawnIndex].type)
+        {
+            case "S":
+                enemyIndex =0;
+                break;
+            case "M":
+                enemyIndex =1;
+                break;
+            case "L":
+                enemyIndex =2;
+                break;    
+        }
+
+        
+        int enemyPoint = spawnList[spawnIndex].point;          
+        GameObject enemy = objectManager.MakeObj(enemyObjs[enemyIndex]);
+        enemy.transform.position = spawnPoints[enemyPoint].position;
 
         Rigidbody2D rigid = enemy.GetComponent<Rigidbody2D>();
         Enemy enemyLogic = enemy.GetComponent<Enemy>();
         enemyLogic.player = player;
         enemyLogic.objectManager = objectManager;
 
-        if (ranPoint == 5 || ranPoint == 6){            //#.Right Spawn
+        if (enemyPoint == 5 || enemyPoint == 6){            //Right Spawn
             enemy.transform.Rotate(Vector3.back*90);
             rigid.velocity = new Vector2(enemyLogic.speed*(-1), -1);
         }
-        else if (ranPoint == 7 || ranPoint == 8){       //#.Left Spawn
+        else if (enemyPoint == 7 || enemyPoint == 8){       //Left Spawn
             enemy.transform.Rotate(Vector3.forward*90);
             rigid.velocity = new Vector2(enemyLogic.speed, -1);
         } 
-        else {      //#. Front Spawn
+        else {      // Front Spawn
             rigid.velocity = new Vector2(0, enemyLogic.speed*(-1));
         }
         
+        spawnIndex++;
+        if(spawnIndex == spawnList.Count)
+        {
+            spawnEnd = true;
+            return;
+        }
+
+        nextSpawnDelay = spawnList[spawnIndex].delay;
     }
 
     public void UpdateLifeIcon(int life)
