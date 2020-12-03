b07abf4df657384afcafbcf1f434ab308611eb16
Min Lee <min.navy.lee@gmail.com>
2020-12-02 21:16:28 -0500

    new main scene

---
+++ FinalProject/Assets/BtnType.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+public class BtnType : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+
+    public BTNType currentType;
+    public Transform buttonScale;
+    Vector3 defaultScale;
+
+
+
+    private void Start()
+    {
+    
+        defaultScale = buttonScale.localScale;
+    
+    }
+
+
+    public void OnBtnClick()
+    {
+        switch(currentType)
+        {
+            case BTNType.New:
+                Debug.Log("New Game");
+                break;
+            case BTNType.Continue:
+                Debug.Log("resume game");
+                break;
+        }
+
+    }
+
+
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        buttonScale.localScale = defaultScale * 1.2f;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        buttonScale.localScale = defaultScale;
+    }
+
+}
