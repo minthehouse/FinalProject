dc84bda4bd5cbe8dd60e31c999f60e05b3b5dc80
Min Lee <min.navy.lee@gmail.com>
2020-12-03 19:25:28 -0500

    connected scenes

--- FinalProject/Assets/Scripts/SceneLoader.cs
+++ FinalProject/Assets/Scripts/SceneLoader.cs
@@ -6,10 +6,21 @@ using UnityEngine.SceneManagement;
 public class SceneLoader : MonoBehaviour
 {
 
-    public void LoadGame()
+    public void LoadGame_1()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
+
+    public void LoadGame_2()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 4);
+    }
+
+        public void LoadGame_3()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 7);
+    }
+
 
     public void QuitGame()
     {
